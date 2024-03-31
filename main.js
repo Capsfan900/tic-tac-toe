6f8b2246aaf13b0ac3876688c8585deb09a6db83
Tyler <tstelly918@gmail.com>
2024-03-31 10:40:45 -0400

    -testing function of setPiece method -testing function of checkForTile method

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 //Dom references 
 
 
-//intial game setup function
+//intial game setup function (objects an function prototypes / methods )
+
+
+//a couple global scope variables
+const board = [];
+const currentPLayer = "X";
 
     const setupGame = () => {
-
             //game object body
             const rows = 3;
             const columns = 3;
              // Create board
-            const board = [];
+    
             for (let row = 0; row < rows; row++) {
             let newRow = [];
                 for (let col = 0; col < columns; col++) {
                  // JS
-                newRow.push(' ');
+                    newRow.push("X");
                 // HTML
              }
                 board.push(newRow);
@@ -33,27 +37,30 @@
         const getBoard = () => board;
         
         const printBoard = () => {
+            //console.clear() make this togglable eventually
             console.log("")
             console.log("Stelly's Console Tic-Tac-Toe"); // Add label before the grid
             console.log("")
             console.log(board.map(row => row.join(' | ')).join('\n---------\n'));
-            console.log("")
+            //let player1 = prompt("Chose Your Name Player (X)");
+            //let player2 = prompt("Chose Your Name Player (O)");
+            console.log("");
+            //console.log(`Player X Name: ${player1}`);
+            //console.log(`Player O Name: ${player2}`);
         }
-
-            return {getBoard, printBoard, getPlayerNames};
+        
+        const resetBoard = () =>{
+            for(let rows = 0; rows < 3; rows++){
+                for(let columns = 0; columns < 3; columns++){
+                    board[rows][columns] = " ";
+                }
+            }
+            console.log("......reseting board")
+        }
+       
+            return {getBoard, printBoard, getPlayerNames,resetBoard};
     };
     
-
-
-
-    
-//instantiate setupGame object 
-const instanceOfSetupGame  = setupGame();
-//destructor methods needs or object dot notation 
-instanceOfSetupGame.getPlayerNames()
-instanceOfSetupGame.printBoard();
-
-
 
 
 
@@ -61,23 +68,66 @@ instanceOfSetupGame.printBoard();
 
     const gameController = () =>{
 
-        const checkForWinner = () => {
+            const checkForTie = () =>{
+                let freeSpaces = 9;
+                for(let i = 0; i < 3; i++)  {
+                    for(let j = 0; j < 3; j++){
+                        console.log(`Checking cell [${i}, ${j}]`);  // Debug log
+                        if(board[i][j] != " "){
+                            freeSpaces--;
+                        console.log(`Found occupied cell at [${i}, ${j}]`);  // Debug log
+                            
+                        }
+                    }
+                }
+                
+            }
             //display winner and trigger board reset
-        }
+        
 
         const setPiece = () => {
-            //set piece depedning on which players turn it is
+            let rowChoice = prompt("enter a row #(1 -3)");
+            let colChoice = prompt("enter a column #(1-3)");
+            console.log(colChoice, rowChoice);
 
         }
 
-        const turnTracker = () =>{
-            //tracks players turn 
-        }
+        const checkForWin = () => {
 
-        
-        return {checkForWinner, setPiece, turnTracker};
+        }
+       
+        return {checkForTie, setPiece, checkForWin};
 
     }
+
+
+
+
+
+
+
+
+    
+//--------------------------program body-------------------------------//
+
+
+
+
+
+//instantiate setupGame and gameController objects
+const instanceOfSetupGame  = setupGame();
+const gameControllerInstance = gameController();
+//destructoring methods using ,dot, object or bracket notation 
+//in this case im using dot notation
+instanceOfSetupGame.getPlayerNames();
+instanceOfSetupGame.printBoard();
+//instanceOfSetupGame.resetBoard();
+instanceOfSetupGame.getBoard();
+instanceOfSetupGame.printBoard();
+gameControllerInstance.checkForTie();
+
+
+
 
 
 
